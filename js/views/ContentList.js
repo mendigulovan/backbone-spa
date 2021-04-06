b28cfa20f7d1c5a01ba9a3833c7d6bb758f3dd6c
Nagima <nagimamendigulova@gmail.com>
2021-04-07 01:43:52 +0200

    correct view

--- js/views/ContentList.js
+++ js/views/ContentList.js
@@ -5,6 +5,8 @@ app.views.ContentList = Backbone.View.extend ({
         this.listenTo(this.collection, "change reset", this.render);
     },
 
+  
+
     render: function(){
         console.log("List: render");
 
@@ -13,18 +15,33 @@ app.views.ContentList = Backbone.View.extend ({
 
         var filmPath = "#category/" + this.collection.catId + "/film/";
 
+       
         this.collection.each(function(model){
-            $ul.append(
-                '<li class="thumb">'+   model.get("title")+              
-                    '<a class="thumb-link" href="'+ filmPath+model.get("id")+'">'+                   
-                        '<span class="overlay"></span>'+
-                        '<img src="">'+
-                    '</a>'+
-                '</li>'
-            );
+            if(model.get("title")){
+                $ul.append(
+                    '<li class="thumb">'+model.get("title")+             
+                        '<a class="thumb-link" href="'+ filmPath+model.get("id")+'">'+                   
+                            '<span class="overlay"></span>'+
+                            '<p>'+model.get("description")+'</p>'+
+                            
+                        '</a>'+
+                    '</li>'
+                );
+            } else {
+                $ul.append(
+                    '<li class="thumb">'+ model.get("name")+             
+                        '<a class="thumb-link" href="'+ filmPath+model.get("id")+'">'+                   
+                            '<span class="overlay"></span>'+
+                            '<p>'+model.get("films")+'</p>'+
+                            '<img src="">'+
+                        '</a>'+
+                    '</li>'
+                );
+            }
         });
 
         
         return this;
-    }
+    },
+
 });
