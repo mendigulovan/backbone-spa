dc63dad662447a42eb95c2c2ae3b9f35e5297ce6
Nagima <nagimamendigulova@gmail.com>
2021-04-07 23:45:40 +0200

    underscore.js

--- js/views/FilmDetail.js
+++ js/views/FilmDetail.js
@@ -1,26 +1,16 @@
-app.views.FilmDetail = Backbone.View.extend({    
+app.views.FilmDetail = Backbone.View.extend({   
+    template: _.template($('script[data-id =film-detail-view]').html()),
+
     initialize: function() {
         this.listenTo(this.model,"change", this.render);
     },
 
     render: function() {
-        this.$el.html(
-            '<header class="film-header l-content l-content-constrained l-row">'+
-                '<div class="l-column thumb-image">'+
-                   '<img src="'+this.model.get("image")+'">'+
-                '</div>'+
-                '<div class="l-column l-padding">'+
-                '<div class="title">'+this.model.get("title")+'</div>'+
-                '<div class="title">'+this.model.get("original_title")+'</div>'+
-                '<div><em>'+this.model.get("director")+'</em> '+this.model.get("release_date")+
-                '<br>'+this.model.get("rt_score")+'</div>'+
-                '</div>'+
-            '</header>'+
-            '<div class="content l-content l-content-constrained standout">'+
-                '<div class="title">Description</div>'+
-                '<p>'+this.model.get("description")+'</p>'+
-            '</div>'
+        this.$el.html( this.template({
+            model: this.model
+        })
         );
+
     return this;
     }
 });
